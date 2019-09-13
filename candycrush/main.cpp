068ad9124a36c992846d1146a451dbca300847e7
Sean <hello@imsean.me>
2019-09-13 16:28:51 -0400

    Complete 1d Candy Crush practice

--- candycrush/main.cpp
+++ candycrush/main.cpp
@@ -1,47 +1,67 @@
 #include <vector>
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
+void printRow (string title, vector<string> row) {
+	string rowString = "";
+	for (const auto &piece : row) rowString += piece + " ";
+
+	cout << title << ": " << endl << rowString << endl;
+}
+
 string randomColor () {
-	char colors = {"R", "G", "B", "O", "p"};
+	const char* colors[] = {"R", "G", "B", "O", "P"};
 	string color = colors[rand() % 5];
 	
 	return color; 
 }
 
-vector<string> parseColorRow (row) {
-	int lastColorIndex;
-	int occurences;
-	int firstOccurenceIndex;
-	
-	for (int i = 0; i < row.size(); i++) {
-		int nextColorIndex = i + 1
-		int color = row[i]		
+vector<string> parseColorRow (vector<string> row) {
+	int index = 0;
+	int occurences = 0;
+	int firstOccurenceIndex = 0;
+	string lastColor;
 
-		if (nextColorIndex <= row.size()) {
-			if (color = row[nextColorIndex]) {
+	while (index < row.size())
+	{
+		string color = row[index];
+		int nextColorIndex = index + 1;
+
+		if (nextColorIndex < row.size()) {
+			if (color == row[nextColorIndex] || color == lastColor) {
 				occurences++;
-				if (occurences == 1) {
-					firstOccurenceIndex = i;
-				} 
-			} else if {
-				occurences = 0
-			}
+				lastColor = row[index];
 
-			if (occurences >= 3) {
-				for (j = firstOccurenceIndex; j <= i; j++) {
-					row[j] = randomColor();
+				if (occurences == 1) {
+					firstOccurenceIndex = index;
 				}
-				parseColorRow(row, )
+
+				if (occurences >= 3) {
+					for (int j = firstOccurenceIndex; j <= index; j++) {
+						row[j] = randomColor();
+					}
+
+					index = firstOccurenceIndex;
+					occurences = 0;
+				}
+			} else {
+				occurences = 0;
 			}
-		} else {
-			
 		}
-	}		
+
+		index++;
+	}
+	
+	return row;
 }
 
 int main () {
-	
+	vector<string> row{"R", "R", "R", "G", "B", "R", "R", "R", "P", "O", "O", "O", "O", "O", "G", "G", "G", "A", "B", "B", "B", "G", "G", "B", "B", "B"};
+	vector<string> parsedRow = parseColorRow(row);
+
+	printRow("Original Row", row);
+	printRow("Parsed Row", parsedRow);
 }
